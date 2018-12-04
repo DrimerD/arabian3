e53dff92dc79583417f4ae06919e49f607c33498
DrimerD <paliyslavik777@gmail.com>
2018-12-05 00:40:23 +0200

    Fixed bugs with sliders

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function ($) {
         $('.top-header .block-search').toggleClass('show-search');
     });
 
-    $('.slider-for').slick({
+    $('#main-slider .slider-for').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
@@ -20,7 +20,7 @@ jQuery(document).ready(function ($) {
         prevArrow: '<button type="button" class="slider-prev"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
         nextArrow: '<button type="button" class="slider-next"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
     });
-    $('.slider-nav').slick({
+    $('#main-slider .slider-nav').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
         asNavFor: '.slider-for',
@@ -30,6 +30,32 @@ jQuery(document).ready(function ($) {
         rtl: true,
     });
 
+    $('#news-slider .slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: false,
+        adaptiveHeight: true,
+        infinite: false,
+        useTransform: true,
+        speed: 400,
+        cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
+        asNavFor: '.slider-nav',
+        autoplay: true,
+        autoPlaySpeed: 5000,
+        rtl:true,
+        prevArrow: '<button type="button" class="slider-prev"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
+        nextArrow: '<button type="button" class="slider-next"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+    });
+    $('#news-slider .slider-nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        focusOnSelect: true,
+        infinite: false,
+        rtl: true,
+    });
 
     $("#latest-news .single-min-news .info-news p a").text(function(i, text) {
 
